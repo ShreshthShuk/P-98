6ad63101ca6ee52ad947697f85cb8408b4693b4c
ShreshthShuk <102597448+ShreshthShuk@users.noreply.github.com>
2022-10-27 20:46:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,10 +5,44 @@ var recognition = new SpeechRecognition;
 function start()
 {
     document.getElementById("textbox").innerHTML = "";
+
     recognition.start();
 }
 
 recognition.onresult = function(event)
 {
     console.log(event);
-}    
+
+    var Content = event.results[0][0].transcript;
+
+    console.log(Content);
+
+    document.getElementById("textbox").innerHTML = Content;
+
+    speak();
+}
+
+camera = document.getElementById("camera");
+
+Webcam.set({
+    width: 360,
+
+    height: 250,
+
+    image_format: 'png',
+
+    png_quality: 90
+});
+
+function speak()
+{
+    var synth = window.speechSynthesis;
+
+    speak_data = document.getElementById("textbox").value;
+    
+    var utterthis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterthis);
+    
+    Webcam.attach(camera);
+}
